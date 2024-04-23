d7ea1624de90c2d8d6cd1fe2032c31588341c89d
Nour Louhichi <nourlouhichi318@gmail.com>
2024-04-23 10:37:07 +0200

    Modifying the VinylMix Entity and make migrations

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -27,7 +27,15 @@ class VinylMix
     private ?string $genre = null;
 
     #[ORM\Column]
-    private ?\DateTimeImmutable $creatAt = null;
+    private \DateTimeImmutable $creatAt ;
+
+    public function __construct()
+    {
+      $this->creatAt=new \DateTimeImmutable();
+    }
+
+    #[ORM\Column]
+    private int $votes = 0;
 
     public function getId(): ?int
     {
@@ -90,6 +98,18 @@ class VinylMix
     public function setCreatAt(\DateTimeImmutable $creatAt): static
     {
         $this->creatAt = $creatAt;
+
+        return $this;
+    }
+
+    public function getVotes(): ?int
+    {
+        return $this->votes;
+    }
+
+    public function setVotes(int $votes): static
+    {
+        $this->votes = $votes;
 
         return $this;
     }
