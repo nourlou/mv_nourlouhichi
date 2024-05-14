ae41e4a6419f15508f805f1efd9839b836b8f06f
Nour Louhichi <nourlouhichi318@gmail.com>
2024-05-14 10:49:07 +0200

    Foundry: Fixtures You'll Love

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,20 +5,22 @@ namespace App\DataFixtures;
 use App\Entity\VinylMix;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\VinylMixFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $mix = new VinylMix();
+      VinylMixFactory::createMany(25);
+      /*  $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
         $genres = ['pop', 'rock'];
         $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
-
         $manager->persist($mix);
-        $manager->flush();
+
+        $manager->flush();*/
     }
 }
